05bad835c40dbb8dbfdaabcb59315d9d37cb25b6
ThesllaDev <theslladev@gmail.com>
2022-08-25 16:19:30 -0300

    ➕Build: install and config sass

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
