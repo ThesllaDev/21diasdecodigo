7099c75f80de19e83b426c74f070dc39fae946e7
ThesllaDev <theslladev@gmail.com>
2022-08-27 23:44:26 -0300

    💄✨Feat: challenge 06 - card with glassmorphism effect

---
+++ pages/challenges/challenge-06/index.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import EquilibriumNFT from "./img/equilibrium.jpg";
+import CreatorAvatar from "./img/creator-avatar.png";
+import IconEthereum from "./img/icon-ethereum.svg";
+import IconClock from "./img/icon-clock.svg";
+
+import styles from "./challenge-06.module.scss";
+
+export default function NFTCard() {
+  return (
+    <main className={styles.main__container}>
+      <section className={styles.main__container__card}>
+        <figure>
+          <Image src={EquilibriumNFT} alt="Cube equilibrium" />
+          <figcaption>Equilibrium #3429</figcaption>
+        </figure>
+        <header className={styles.main__container__card__description}>
+          Our Equilibrium collection promotes balance and calm.
+        </header>
+        <section className={styles.main__container__card__infos}>
+          <span className={styles.main__container__card__infos__crypto}>
+            <Image src={IconEthereum} alt="Icon of ethereum" /> 0.041 ETH
+          </span>
+          <span className={styles.main__container__card__infos__time}>
+            <Image src={IconClock} alt="Icon of clock" /> 3 days left
+          </span>
+        </section>
+        <hr />
+        <footer className={styles.main__container__card__creator}>
+          <div className={styles.main__container__card__creator__avatar}>
+            <Image src={CreatorAvatar} alt="NFT creator avatar profile" />
+          </div>
+          <h2 className={styles.main__container__card__creator__about}>
+            Creation of <a href="#">Jules Wyvern</a>
+          </h2>
+        </footer>
+      </section>
+    </main>
+  );
+}
